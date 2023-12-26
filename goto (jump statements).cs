eb1c110343d976e0d569c72301e2b9f65f9d2334
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-25 19:04:41 -0500

    goto

---
+++ goto (jump statements).cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Loop from 0 to 10
+        for (int i = 0; i <= 10; i++)
+        {
+            // Check if i is equal to 5
+            if (i == 5)
+                // Jump to the label "Stop" if i is equal to 5
+                goto Stop;
+
+            // Print the current value of i
+            Console.WriteLine(i);
+        }
+
+        // This block will be ignored because of the goto statement
+        {
+            Console.WriteLine("loop terminates..");
+        }
+
+    // Label indicating where the goto statement will jump
+    Stop:
+        {
+            // Print this message when the program encounters the "Stop" label
+            Console.WriteLine("program exits..");
+        }
+    }
+}
