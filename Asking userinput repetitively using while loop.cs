cd449312e39e4a0ac4f2b429b4666593092952ef
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-27 02:10:24 -0500

    userinput continues...

---
+++ Asking userinput repetitively using while loop.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+public class Learning
+{
+    public static void Main(string[] args)
+    {
+        while(true)
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            int multiply = num1 * num2;
+            Console.WriteLine("The multiplication is:" + multiply);
+            Console.WriteLine("Do you want to restart your program:" + "Yes/No");
+            string userinput = Console.ReadLine().ToLower();
+            if (userinput == "yes")
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+                
+          
+    }
+
+}
+
