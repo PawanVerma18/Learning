2a1bf7b9ab310e912715e299a1fd446a1381a788
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-27 01:56:31 -0500

    free time practice

---
+++ Asking for userinput again and repetitively using do-while loops.cs
@@ -0,0 +1,20 @@
+﻿public class Learning
+{
+    public static void Main(string[] args)
+    {
+        string userinput;
+        do
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("The Sum is:" + sum);
+            Console.WriteLine("Do you want to restart your program:" + "Yes/No");
+            userinput = Console.ReadLine().ToLower();
+        }
+        while (userinput == "yes");
+    }
+
+}
