fecb8c07d2f8319feac108bd2a988c119776b84c
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-27 01:35:01 -0500

    do while loops

---
+++ do-while loop.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Intrinsics.Arm;
+
+public class Pawan
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any number:");
+        int x=Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+        do
+        {
+            Console.WriteLine(i*x);
+            i++;
+        }
+        while (i<=10);
+    }
+}
