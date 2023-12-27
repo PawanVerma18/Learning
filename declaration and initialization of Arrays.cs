e7a149493e443b61546a3e3ed4c4d2c6cb7c0523
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-27 15:07:42 -0500

    Arrays

---
+++ declaration and initialization of Arrays.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.NetworkInformation;
+
+public class arrays
+{
+    public static void Main(string[] args)
+    {
+        //Type 1
+
+        string[] my_array1 = { "Krishna", "Yash", "John", "sam" };
+
+        {
+            Console.WriteLine(my_array1[3]);
+        }
+
+
+        //Type 2
+
+        string[] my_array2 = new string[] { "Krishna", "Pawan", "Sam", "Sahil" };
+        {
+            Console.WriteLine(my_array2[0]);
+        }
+
+
+
+        //Type 3
+
+        string[] my_array3 = new string[4];
+        my_array3[0] = "Pawan";
+        my_array3[1] = "sam";
+        my_array3[2] = "krishna";
+        my_array3[3] = "sahil";
+        {
+            Console.WriteLine(my_array3[0]);
+        }
+
+    }
+}
