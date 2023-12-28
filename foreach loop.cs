3b937332be6a945a00df67ec4b4003095904cfe4
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-27 21:18:22 -0500

    learning foreach loop

---
+++ foreach loop.cs
@@ -0,0 +1,25 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        //int data type
+
+        int[] numbers = { 1, 23, 67, 989, 74, 36 };
+        foreach(int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        Console.WriteLine("------------------------");
+
+
+        //  String data type
+
+        string[] names = new string[] { "Pawan", "Sam", "krishna", "sahil", "yash" };
+        foreach(string item in names)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
