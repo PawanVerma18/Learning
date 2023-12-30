7ebf08ca9a2807d33dc53ab7a03681ba20c7d621
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-29 23:49:07 -0500

    ok

---
+++ ARRAYS/Initializing array with userinput.cs
@@ -0,0 +1,20 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("How many numbers you want in your array:");
+        int num=Convert.ToInt32(Console.ReadLine());
+        int[] items = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine("Enter data at location:"+(i+1));
+            int data=Convert.ToInt32(Console.ReadLine());
+            items[i] = data;
+        }
+        Console.WriteLine("-----------Your Data------------");
+        foreach (int value in items)
+        {
+            Console.Write(value + " ");
+        }
+    }
+}
