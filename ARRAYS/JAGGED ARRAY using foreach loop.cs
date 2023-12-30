213ef7a4f437dadf65691612630e7160d606ca6a
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-29 21:56:13 -0500

    12

---
+++ ARRAYS/JAGGED ARRAY using foreach loop.cs
@@ -0,0 +1,19 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        int[][] numbers = new int[3][];
+        numbers[0] = new[] { 1, 2, 25, 56 };
+        numbers[1] = new[] { 3, 2, 15, 56 };
+        numbers[2] = new[] { 2, 2, 45, 67 };
+        foreach (int[] items in numbers)
+        {
+            foreach(int i in items)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
