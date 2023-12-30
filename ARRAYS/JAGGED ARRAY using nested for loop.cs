f46543da1465864a91766ad5e27d8a1ca6b02b2a
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2023-12-29 22:18:49 -0500

    1

---
+++ ARRAYS/JAGGED ARRAY using nested for loop.cs
@@ -0,0 +1,20 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        int[][] numbers = new int[3][];
+        numbers[0] = new[] { 1, 2, 3 };
+        numbers[1] = new[] { 12, 31, 45, 56, 56 };
+        numbers[2] = new[] { 12, 33, 21, 32, 43, 43, 98, 99 };
+        for(int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for(int j = 0; j < numbers[i].Length; j++)
+            {
+                Console.Write(numbers[i][j] + " ");
+            }
+            Console.WriteLine();
+
+        }
+
+   }
+}
