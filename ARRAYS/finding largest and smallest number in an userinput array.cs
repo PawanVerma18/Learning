6e5f75a37f23f630fc57fa2000d024fe0beabf70
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-16 02:36:25 -0500

    3r34

---
+++ ARRAYS/finding largest and smallest number in an userinput array.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.Metrics;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        const int maxsize = 10000;
+        int[] numbers = new int[maxsize];
+        int count = 0;
+        int largest = int.MinValue;
+        int smallest= int.MaxValue;
+
+        Console.WriteLine("Enter your numbers: (Enter -1 to terminate!)");
+
+        int userinput;
+        while((userinput = Convert.ToInt32(Console.ReadLine())) != -1 && count<maxsize)
+        {
+            if(userinput>=0)
+            {
+                if (userinput > largest)
+
+                    largest = userinput;
+
+
+                if (userinput < smallest)
+
+                    smallest = userinput;
+
+                numbers[count] = userinput;
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Negative number ignored!");
+            }
+
+
+        }
+
+        Console.Write("\nEntered numbers are:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"\nTotal numbers entered:"+ count);
+        Console.WriteLine($"\nLargest number:"+largest);
+        Console.WriteLine($"\nSmallest number:"+smallest);
+
+
+    }
+}
