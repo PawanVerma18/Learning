84cb7942d6dd64a69fe2e41230c17a9dab5c2430
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-17 17:42:03 -0500

    converter

---
+++ Small console projects/TemperatureConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp44;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TemperatureConverter converter = new TemperatureConverter();
+        Console.WriteLine("Choose the Option you want to convert temperature from:");
+        Console.WriteLine("Enter 1 for Celsius to Fahrenheit:");
+        Console.WriteLine("Enter 2 for Fahrenheit to Celsius:");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        if(input == 1)
+        {
+            Console.WriteLine("Enter your Temperature in Celcius:");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            converter.CtoF(temp);
+        }
+
+        else if(input == 2)
+        {
+            Console.WriteLine("Enter your Temperature in Fahrenheit:");
+            double tepm=Convert.ToDouble(Console.ReadLine());
+            converter.FtoC(tepm);
+        }
+  
+
+
+
+    }
+}
---
+++ Small console projects/TemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    internal class TemperatureConverter
+    {
+       public void CtoF(double celsius)
+        {
+            double Farhenheit= (celsius * 9 / 5) + 32;
+            Console.WriteLine($"Temperature in Fahrenheit: {Farhenheit}");
+        }
+
+        public void FtoC(double fahrenheit)
+        {
+            double Celsius= (fahrenheit -32) * 5/ 9;
+            Console.WriteLine($"Temperature in Celsius: {Celsius}");
+        }
+    }
+}
