47f34917cc86785f7ed7ea84c00543633e783621
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-18 00:44:24 -0500

    return type

---
+++ Small console projects/Bank Account C#/BankAccount_PrivateAttributes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    internal class BankAccount
+    {
+        private String AccountNumber;
+        private double Balance;
+
+        public BankAccount(string a, double b)
+        {
+            AccountNumber = a;
+            Balance = b;
+        }
+
+        public string GetAccountNumber()
+        {
+            return AccountNumber;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+        public void deposit(double amount)
+        {
+            if(amount>0)
+            {
+                Balance = Balance + amount;
+                Console.WriteLine($"Amount {amount} is credited successfully!");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount!");
+            }
+            
+        }
+
+        public void withdrawal(double amount)
+        {
+            if(amount>0 && amount==Balance)
+            {
+                Balance = Balance - amount;
+                Console.WriteLine($"Amount {amount} is debited successfully!");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient Balance!");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Invalid Amount!");
+            }
+        }
+
+    }
+
+}
---
+++ Small console projects/Bank Account C#/Programbank_PrivateAttributes.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp46;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        BankAccount a1 = new BankAccount("45493000180", 2000);
+        Console.WriteLine($"AccounNumber:{a1.GetAccountNumber()} has {a1.GetBalance()} total balance!");
+
+        a1.deposit(1000);
+        Console.WriteLine($"AccounNumber:{a1.GetAccountNumber()} has {a1.GetBalance()} total balance!");
+
+        a1.withdrawal(34500);
+        Console.WriteLine($"AccounNumber:{a1.GetAccountNumber()} has {a1.GetBalance()} total balance!");
+    }
+}
