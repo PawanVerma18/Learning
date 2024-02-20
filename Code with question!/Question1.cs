436a588576ceaf7dec52704bf3e69fe435b39a1f
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-20 04:55:13 -0500

    qw

---
+++ Code with question!/Question1.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+//Write a program that prompts the user to enter a set of numbers terminated by -1. The program must then print all the numbers entered by the user at the end of the program and then print the number of numbers entered by the user and the largest and smallest number within the set. Program should store all the numbers in an array called numbers. 
+//Hints: Assume there are at most 10,000 numbers entered
+
+public class Array
+{
+    
+    public static void Main(string[] args)
+    {
+        const int maxsize = 10000;
+        int[] numbers = new int[maxsize];
+        int count = 0;
+        int largestvalue = int.MinValue;
+        int smallestvalue = int.MaxValue;
+
+        Console.WriteLine("Enter your data: (Enter -1 to eliminate!)");
+
+        while (count < maxsize)
+        {
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            if (data == -1)
+            {
+                break;
+            }
+
+            if (data >= 0)
+            {
+                numbers[count] = data;
+                count++;
+
+                if (largestvalue < data)
+                {
+                    largestvalue = data;
+                }
+
+                if (smallestvalue > data)
+                {
+                    smallestvalue = data;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Negative numnber ignored!");
+            }
+
+
+        }
+
+        Console.Write("\nYour data:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+
+        Console.Write("\nTotal number entered:"+count);
+
+        Console.Write("\nLagest number:" + largestvalue);
+        Console.Write("\nSmallest number:"+smallestvalue);
+    }
+}
--- small projects just for fun/PrintPrimeNum and check if it a primeNum or not.cs
+++ small projects just for fun/PrintPrimeNum and check if it a primeNum or not.cs
@@ -1,65 +1,23 @@
 ﻿public class Program
 {
-    public static bool PrintPrimeNum(int x)
-    {
-        if (x <= 1) return false;
-        if (x == 2 || x == 3) return true;
-        if (x % 2 == 0 || x % 3 == 0) return false;
-
-        int i = 5;
-        while (i * i <= x)
-        {
-            if (x % i == 0 || x * (i + 2) == 0)
-                return false;
-            i = i + 6;
-        }
-        return true;
-    }
-
-        public static bool IsPrime(int x)
-    {
-        if (x <= 1) return false;
-        if(x==2 ||  x==3) return true;
-        if (x % 2 == 0 || x % 3 == 0) return false;
-
-        int i = 5;
-        while (i * i <= x)
-        {
-            if(x%i==0 || x%(i+2) == 0)
-                return false;
-            i = i + 6;
-        }
-        return true;
-
-    }
     
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a range:");
-        Console.Write("From:");
-        int a =Convert.ToInt32(Console.ReadLine());
-        Console.Write("TO:");
-        int b = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[10];
+        int sum =0;
+        Console.WriteLine("Enter your data:");
+        for (int i = 0; i <= 10; i++)
+        {
+           int data = Convert.ToInt32(Console.ReadLine());
+            numbers[i] = data;
+            sum = sum + data;
+        }
+        Console.Write("Your Data:");
+        foreach (int value in numbers)
+        {
+            Console.Write(value + " ");
+        }
 
-        Console.Write($"Prime Numbers between {a} and {b} are:");
-        
-        
-            for(int x = a; x < b; x++)
-            {
-               if (PrintPrimeNum(x))
-                Console.Write(x + " ");   
-
-            }
-                
-        
-        Console.WriteLine();
-
-        Console.Write("Enter a number to check is it is prime number or not:");
-
-        int c = Convert.ToInt32(Console.ReadLine());
-        bool z =IsPrime(c);
-        Console.WriteLine(z);
-
-
+        Console.WriteLine("Sum:"+sum);
     }
 }
