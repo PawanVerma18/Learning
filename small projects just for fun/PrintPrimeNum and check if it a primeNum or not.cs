dadb678652d551401e47000b0329c827098625a6
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-19 21:33:38 -0500

    11

---
+++ small projects just for fun/PrintPrimeNum and check if it a primeNum or not.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    public static bool PrintPrimeNum(int x)
+    {
+        if (x <= 1) return false;
+        if (x == 2 || x == 3) return true;
+        if (x % 2 == 0 || x % 3 == 0) return false;
+
+        int i = 5;
+        while (i * i <= x)
+        {
+            if (x % i == 0 || x * (i + 2) == 0)
+                return false;
+            i = i + 6;
+        }
+        return true;
+    }
+
+        public static bool IsPrime(int x)
+    {
+        if (x <= 1) return false;
+        if(x==2 ||  x==3) return true;
+        if (x % 2 == 0 || x % 3 == 0) return false;
+
+        int i = 5;
+        while (i * i <= x)
+        {
+            if(x%i==0 || x%(i+2) == 0)
+                return false;
+            i = i + 6;
+        }
+        return true;
+
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a range:");
+        Console.Write("From:");
+        int a =Convert.ToInt32(Console.ReadLine());
+        Console.Write("TO:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"Prime Numbers between {a} and {b} are:");
+        
+        
+            for(int x = a; x < b; x++)
+            {
+               if (PrintPrimeNum(x))
+                Console.Write(x + " ");   
+
+            }
+                
+        
+        Console.WriteLine();
+
+        Console.Write("Enter a number to check is it is prime number or not:");
+
+        int c = Convert.ToInt32(Console.ReadLine());
+        bool z =IsPrime(c);
+        Console.WriteLine(z);
+
+
+    }
+}
