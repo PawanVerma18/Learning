504395a03b58fe3344c198efbe4b5f67ac128b50
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-25 22:30:50 -0500

    12

---
+++ Constructor/CopyConstructor.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace CopyConstructor
+{
+    class Person
+    {
+        public  int Age;
+        public string Name;
+
+        public Person(int a,string n)
+        {
+            Age = a;
+            Name = n;
+        }
+
+        public void details()
+        {
+            Console.WriteLine("Name is:"+Name);
+            Console.WriteLine("Age is:"+ Age);
+        }
+
+        public Person(Person p) //copy constructor
+        {
+            Age = p.Age;
+            Name = p.Name;
+        }
+
+       
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(21,"Pawan");
+            Person p2 = new Person(p1);
+            p1.details();
+            Console.WriteLine();
+            p2.details();   
+
+        }
+    }
+}
---
+++ Encapsulation/program1 get set.cs
@@ -0,0 +1,71 @@
+﻿namespace AccessSpecifiers1
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public void setdetails(string n,int a)
+        {
+            if (string.IsNullOrEmpty(n)==true)
+            {
+                Console.WriteLine("Name cannot be empty!");
+            }
+            else
+            {
+                name  = n;
+            }
+
+            if (a> 0)
+            {
+                age = a;
+            }
+            else
+            {
+                Console.WriteLine("Age cannot be Negative or Zero!");
+            }
+        }
+        public void getdetails()
+        {
+            if (string.IsNullOrEmpty(name) == true)
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("Name is:"+name);
+            }
+
+            if(age > 0)
+            {
+                Console.WriteLine("Age is:"+age);
+            }
+            else
+            {
+
+            }
+            
+
+        }
+
+
+
+    }
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            Console.WriteLine("Enter your name:");
+            string x = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            p1.setdetails(x,y);
+            p1.getdetails();
+            
+        }
+
+    }
+}
