800cbb013b0747228b0c9ec3013d091bd39f98f7
PawanVerma18 <154846635+PawanVerma18@users.noreply.github.com>
2024-02-29 23:29:13 -0500

    12

---
+++ Abstraction/Program1.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace ConsoleApp56
+{
+    public abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int age;
+        public long PhoneNo;
+
+        public abstract void Show();
+
+
+    }
+
+    class Student : Person
+    {
+        public int RollNo;
+        public int Fees;
+
+        public override void Show()
+        {
+            string Name = this.FirstName + " " + this.LastName;
+            Console.WriteLine("----------Student Details Are---------");
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Age:" + this.age);
+            Console.WriteLine("PhoneNo:"+ this.PhoneNo);
+            Console.WriteLine("RollNo:"+ this.RollNo);
+            Console.WriteLine("Fees:"+this.Fees);
+
+        }
+    }
+
+    class Teacher : Person 
+    {
+        public string Qualification;
+        public int salary;
+
+        public override void Show()
+        {
+            string Name = this.FirstName + " " + this.LastName; 
+            Console.WriteLine("----------Teacher Details Are----------");
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Age:" + this.age);
+            Console.WriteLine("PhoneNo:" + this.PhoneNo);
+            Console.WriteLine("Qualification:" + this.Qualification);
+            Console.WriteLine("Salary:" + this.salary);
+
+
+        }
+    }
+   
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.FirstName = "Pawan";
+            s1.LastName = "Verma";
+            s1.age = 21;
+            s1.PhoneNo = 4375561538;
+            s1.RollNo = 27;
+            s1.Fees = 6000;   
+            
+            s1.Show();
+
+            Console.WriteLine();
+
+            Teacher t1 = new Teacher();
+            t1.FirstName = "Anil";
+            t1.LastName = "Singh";
+            t1.age = 40;
+            t1.PhoneNo = 9140298218;
+            t1.Qualification = "Phd in Physics";
+            t1.salary = 90000;
+
+            t1.Show();
+
+
+        }
+    }
+
+}
---
+++ Abstraction/UsingProperties.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace ConsoleApp57
+{
+    abstract class Person
+    {
+        public abstract int id { get; set; }
+        public abstract string name { get; set; }
+    }
+
+    class Student : Person
+    {
+        private int Stdid;
+        private string Stdname;
+
+        public override int id
+        {
+            set
+            {
+                if(value > 0)
+                {
+                    this.Stdid = value;
+
+                }
+                else
+                {
+                    throw new Exception(" Invalid Id!");
+                }
+            }
+
+            get
+            {
+                return this.Stdid;
+            }
+        }
+
+        public override string name
+        {
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new Exception(" Name Cannot be Null!");
+
+                }
+                else
+                {
+                   this.Stdname = value;
+                }
+            }
+
+            get
+            {
+                return this.Stdname;
+
+            }
+        }
+     
+
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            
+
+            try
+            {
+                s1.id = 1;
+                s1.name = "Pawan";
+                Console.WriteLine("ID: " + s1.id);
+                Console.WriteLine("Name: " + s1.name);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Occured:" + e.Message);
+            }
+        }
+    }
+}
